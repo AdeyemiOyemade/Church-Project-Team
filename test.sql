6d13b6a3ac2ab45290efa4c6f54f5521da6b5a34
amakachi93 <54386159+amakachi93@users.noreply.github.com>
2019-08-28 23:07:44 -0400

    Test Chiamaka

--- test.sql
+++ test.sql
@@ -5,8 +5,4 @@ CREATE TABLE test (
 );
 
 INSERT INTO test (id, name) VALUES (1, Adeyemi);
-<<<<<<< HEAD
 INSERT INTO test (id, name) VALUES (2, Chiamaka);
-=======
-INSERT INTO test (id, name) VALUES (2, Adeyemi);
->>>>>>> e1f21f558a842f19db142e3fca565ac203ccc223
