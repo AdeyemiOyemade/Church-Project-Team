712ccddcc9b0580a4705e331021d5536a1241f45
JayBoyHub <54386667+JayBoyHub@users.noreply.github.com>
2019-08-28 23:25:32 -0400

    Update test.sql
    INSERT INTO test (id, name) VALUES (4, Jerry);

--- test.sql
+++ test.sql
@@ -7,3 +7,4 @@ CREATE TABLE test (
 INSERT INTO test (id, name) VALUES (1, Adeyemi);
 INSERT INTO test (id, name) VALUES (2, Chiamaka);
 Insert INTO test (id, name) VALUES (3, Melissa);
+INSERT INTO test (id, name) VALUES (4, Jerry);
