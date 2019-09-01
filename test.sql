666bd024221e87c8c38eeeaf3a87912c9f823893
Dave <amoakodavid@ymail.com>
2019-08-31 20:09:32 -0400

    DAVID INSERT

--- test.sql
+++ test.sql
@@ -8,3 +8,4 @@ INSERT INTO test (id, name) VALUES (1, Adeyemi);
 INSERT INTO test (id, name) VALUES (2, Chiamaka);
 Insert INTO test (id, name) VALUES (3, Melissa);
 INSERT INTO test (id, name) VALUES (4, Jerry);
+INSERT INTO test (id, name) VALUES (5, DAVID);
